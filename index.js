115686e0502c70ff8384f37e507ac7a8e5d530d9
lpahl <pahlischl@gmail.com>
2023-07-29 09:55:09 +0200

    fix order_id

--- index.js
+++ index.js
@@ -171,9 +171,11 @@ app.post('/order/insert', async function (req, res) {
             await insertOrder(req); 
             existing_order = await getOrderByMemberIdAndDate(cobot_member_id, order_date);
         }
+
+        let order_id;
         if (existing_order.length !== 0) {
-            let order_id = existing_order[0].id;
-            console.log(order_id)
+            order_id = existing_order[0].id;
+            
         }
         await insertOrderDetails(order_id, req); 
 
