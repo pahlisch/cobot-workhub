7fbee2f6e5492d12189e5a1c24c4004ce1a1fc86
lpahl <pahlischl@gmail.com>
2023-10-01 08:15:56 +0200

    smtp transport

--- index.js
+++ index.js
@@ -31,6 +31,16 @@ console.log(dbConfig)
 const dbUrl = process.env.CLEARDB_DATABASE_URL;
 
 
+const smtpTransport = nodemailer.createTransport({
+  host: process.env.MAILGUN_SMTP_SERVER,
+  port: process.env.MAILGUN_SMTP_PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAILGUN_SMTP_LOGIN,
+    pass: process.env.MAILGUN_SMTP_PASSWORD
+  }
+});
+
 
 
 async function getMealItems() {
